e7f85b9cfff3816f5bddbc6149689bdc68cae25f
Zăn Zăn <69894094+Van-IoT2020@users.noreply.github.com>
2021-06-09 17:54:29 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,6 @@ Route::get('/admin/', function () {
     return view('Admin.welcome');
 });
 
-Route::get('/admin/home', function () {
-    return view('Admin.welcome');
-});
+// Route::get('/admin/home', function () {
+//     return view('Admin.welcome');
+// });
